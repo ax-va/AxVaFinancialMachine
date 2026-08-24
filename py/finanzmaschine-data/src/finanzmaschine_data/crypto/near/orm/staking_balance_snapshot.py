967b6b7527007372dd07d084bba4dfdb3bf0ee7e
ax-va <vasiliev.informatik@t-online.de>
2026-08-24 13:39:10 +0200

    updated

---
+++ py/finanzmaschine-data/src/finanzmaschine_data/crypto/near/orm/staking_balance_snapshot.py
@@ -0,0 +1,41 @@
+from pydantic import field_validator
+from sqlalchemy import BigInteger
+from sqlmodel import Field, SQLModel
+
+
+class StakingBalanceSnapshot(SQLModel, table=True):
+    __tablename__ = "staking_balance_snapshots"
+
+    account_id: str = Field(primary_key=True)
+    pool_id: str = Field(primary_key=True)
+
+    block_height: int = Field(
+        primary_key=True,
+        foreign_key='block_heights.block_height',
+        sa_type=BigInteger,
+    )
+
+    staked_balance_yocto_str: str
+    unstaked_balance_yocto_str: str
+
+    @field_validator(
+        'staked_balance_yocto_str',
+        'unstaked_balance_yocto_str',
+    )
+    @classmethod
+    def validate_balance_yocto_str(cls, value: str) -> str:
+        if not value.isdigit():
+            raise ValueError(f"Balance yocto sting must contain only digits: {value}")
+        return value
+
+    @property
+    def staked_balance_yocto(self) -> int:
+        return int(self.staked_balance_yocto_str)
+
+    @property
+    def unstaked_balance_yocto(self) -> int:
+        return int(self.unstaked_balance_yocto_str)
+
+    @property
+    def total_balance_yocto(self) -> int:
+        return self.staked_balance_yocto + self.unstaked_balance_yocto
